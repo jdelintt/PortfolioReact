4fa86fd15c19c7ccdb97cc1dde233c720d54ce14
Jesus DeLintt <jesus.delintt@gmail.com>
2020-10-11 13:38:38 -0700

    created a nav bar and grid container

--- portfolioreact/src/App.js
+++ portfolioreact/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Navbar from './Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <Navbar></Navbar>
+    <CssBaseline />
+    <Container maxWidth="sm">
+      <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+    </Container>
+  </React.Fragment>
   );
 }
-
 export default App;
